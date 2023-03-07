87ea7f174723d88fa8bc1afee7568fba6cf069fc
Terezija Semenski <terezija.semenski@gmail.com>
2023-03-07 11:23:38 +0100

    Adding subtract

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
         return a + b;
     }
 
+    public static double subtract(double a, double b){
+        return a - b;
+    }
+
     public static double square(double a){
         return a*a;
     }
