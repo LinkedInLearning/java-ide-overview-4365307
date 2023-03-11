3a43ebdbbc85ea9282a55e688bbbe9254d2af41f
Terezija Semenski <terezija.semenski@gmail.com>
2023-03-11 20:03:58 +0100

    Adding DebugDemo and Calculator fix

---
+++ src/main/java/org/example/demo/DebugDemo.java
@@ -0,0 +1,31 @@
+package org.example.demo;
+
+public class DebugDemo {
+    public static void main(String[] args) {
+
+        SampleObject sample= new SampleObject();
+
+        int sum=0;
+        for (int i = 0; i < 200; i++) {
+            
+            //how to stop when sum goes beyond 100?
+            //how to stop after 10 times?
+            sum+=i;
+
+
+            //stop on value change
+            sample.value=sum;
+
+            //stop on exception
+            if(sum>400)
+            {
+                //throw new IllegalStateException("Demo of exception");
+            }
+            
+        }
+    
+    }
+}
+class SampleObject {
+    int value;
+}
