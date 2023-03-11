d3cc352ba52e10233e07553451fdfa2a3b3068f0
Terezija Semenski <terezija.semenski@gmail.com>
2023-03-11 11:53:49 +0100

    Adding product and code samples

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.List;
+
+import org.example.model.Product;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        List<Integer> nums=List.of(1,3,6,7);
+        
+        for (Integer num : nums) {
+			
+		}
+        
+        if (nums == null) {
+			
+		}
+        
+        try {
+        	nums.get(5);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        	// TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        
+        new Product("iphone",1000.0);
+        
+        
+        
     }
 }
